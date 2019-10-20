5f1e04eda9c9a68218f2d890eaf05aa4cdb60cca
mehmet <mehmetccm@gmail.com>
2019-10-20 17:16:45 +0300

    minor changes

--- test.c
+++ test.c
@@ -2,59 +2,33 @@
 #include <stdio.h>
 #include <time.h>
 #include <sys/time.h>
-#include <limits.h>
+#include <stdlib.h>
+#include <memory.h>
 
-#define LARGE 256
+int read_from_file() {
+       static const char filename[] = "input.fa";
+   FILE *file = fopen ( filename, "r" );
+   char *dest[2048];
+   if ( file != NULL )
+   {
+      char line [ 128 ]; /* or other suitable maximum line size */
+      while ( fgets ( line, sizeof line, file ) != NULL ) /* read a line */
+      {
+         strcat(dest,line);
+      }
 
-int rabin_karp(char *input, char *pattern, int heuristic, int large)
-{
-    int M = strlen(pattern);
-    int N = strlen(input);
-    int i, j;
-    int p = 0;
-    int t = 0;
-    int h = 1;
+      printf("%s", dest);
+      fclose ( file );
+   }
+   else
+   {
+      perror ( filename ); /* why didn't the file open? */
+   }
+   return 0;
 
-    for (i = 0; i < M - 1; i++)
-        h = (h * large) % heuristic;
 
-    for (i = 0; i < M; i++)
-    {
-        p = (large * p + pattern[i]) % heuristic;
-        t = (large * t + input[i]) % heuristic;
-    }
-
-    for (i = 0; i <= N - M; i++)
-    {
-
-        if (p == t)
-        {
-            for (j = 0; j < M; j++)
-            {
-                if (input[i + j] != pattern[j])
-                    break;
-            }
-
-            if (j == M)
-                return i;
-        }
-
-        if (i < N - M)
-        {
-            t = (large * (t - input[i] * h) + input[i + M]) % heuristic;
-
-            if (t < 0)
-                t = (t + heuristic);
-        }
-    }
-
-    return -1;
 }
 
 int main() {
-    char *input = "TAAGTCTATACCATCGTAGTCTAATTAACGTTATGGTAGGATATCAAGGACGGAATGACCGCAGAGGCGACGTTAATGCGCCGTCAGAGACGCCCTAAAGATTGCGGTAGGGTCCCGTTGTTAAAGAGACTTGAGTGGGTGCTTGATGGGAGTGTATTAAGGGCATGTATAAGTGTTGCTGGGTCTAAGGCATTAAAGCTGAGTCAATAGTTACATTGCAGATTAACGAGATCTGAAATTAAGGGAGAGATTCCCAGAGTGGCCTAGTACTTAAGGGCACCCACGCCGCAGGCGGCCCTACGCCCGTTAATGGTTCGAGTGCTATTCACTAACACATTAACGGACGTTTAGTGTGGATTATAGGTGAAGGGTCTGCGCCACTCCAAGGCAGGGAACATATGTGTTGTTACTATCTTAACG";
-    char *pattern = "TGGGTCTAAGGCATTAAAGCTGAGTCAATAGT";
-
-    int found = rabin_karp(input, pattern, 97, 256);
-    printf("found at %d\n", found);
+    read_from_file();
 }
